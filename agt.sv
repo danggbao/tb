15e614d005c250570b3c3e54d3821857d5aca34d
danggbao <dangbao290404@gmail.com>
2026-04-13 03:57:43 +0700

    feat: update agent constructor to include completion event and implement transaction forwarding in run task

--- agt.sv
+++ agt.sv
@@ -3,12 +3,19 @@ class agent;
     mailbox agt2drv;
     mailbox agt2scb;
     event  agent_done;
-    function new(mailbox gen2agt, mailbox agt2drv, mailbox agt2scb);
+    function new(mailbox gen2agt, mailbox agt2drv, mailbox agt2scb, event agent_done);
         this.gen2agt = gen2agt;
         this.agt2drv = agt2drv;
         this.agt2scb = agt2scb;
+        this.agent_done = agent_done;
     endfunction
     task run();
-        
+        transaction tr;
+        forever begin
+            gen2agt.get(tr);
+            agt2drv.put(tr);
+            agt2scb.put(tr);
+            -> agent_done;
+        end
     endtask
 endclass
