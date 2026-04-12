07a103cc90fbb6e87bcae11b7d51aae186c5d5b7
danggbao <dangbao290404@gmail.com>
2026-04-13 03:59:18 +0700

    refactor: localize transaction object and add randomization failure check in generator

--- generator.sv
+++ generator.sv
@@ -1,16 +1,19 @@
 class generator;
     mailbox gen2agt;
-    transaction tr;
+    
     event agent_done;
     int repeat_count = 20;
     function new(mailbox gen2agt, event ev);
         this.gen2agt = gen2agt;
-        this.agent_done = ev; = ev;
+        this.agent_done = ev;
     endfunction
     task run();
+        transaction tr;
         repeat(repeat_count) begin
             tr = new();
-            tr.randomize();
+            if(!tr.randomize()) begin
+                $display("[gen] randomize failed");
+            end
             gen2agt.put(tr);
             @(agent_done);
         end
