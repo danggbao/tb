c43801ee7b8cf2eb3af37912807ba934010e99d9
danggbao <dangbao290404@gmail.com>
2026-04-13 04:32:54 +0700

    feat: add monitor class and update agent to synchronize with driver via event trigger

--- agt.sv
+++ agt.sv
@@ -3,11 +3,14 @@ class agent;
     mailbox agt2drv;
     mailbox agt2scb;
     event  agent_done;
-    function new(mailbox gen2agt, mailbox agt2drv, mailbox agt2scb, event agent_done);
+    event  drv_done;
+
+    function new(mailbox gen2agt, mailbox agt2drv, mailbox agt2scb, event agent_done, event drv_done);
         this.gen2agt = gen2agt;
         this.agt2drv = agt2drv;
         this.agt2scb = agt2scb;
         this.agent_done = agent_done;
+        this.drv_done = drv_done;
     endfunction
     task run();
         transaction tr;
@@ -16,7 +19,7 @@ class agent;
             agt2drv.put(tr);
             agt2scb.put(tr);
             -> agent_done;
-            display("[agt] put done")
+            $display("[agt] put done");
             @(drv_done);
         end
     endtask
